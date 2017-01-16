7dba13471aabc4289daab86189472641bd0abd90
Mason <mason.sciotti@p97.com>
2017-01-15 20:27:54 -0500

    BlockBreaker game begin. Basic physics

---
+++ BlockBreaker/Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+	public LevelManager _levelmanager;
+	void OnCollisionEnter2D(Collision2D collision){
+		
+	
+	}
+	
+	void OnTriggerEnter2D(Collider2D collider){
+		_levelmanager.LoadLevel("Win Screen");
+	
+	}
+}
---
+++ BlockBreaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	static MusicPlayer _musicPlayer = null;
+	
+	void GetInstance(){
+		if (_musicPlayer == null){
+			_musicPlayer = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+		else Destroy(gameObject);
+	}
+	
+	void Awake(){
+		GetInstance();
+	}
+}
