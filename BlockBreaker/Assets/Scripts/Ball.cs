7606a21487048309a03a0a1ddf0bb5d68d4041d3
Mason Sciotti <mason.sciotti@gmail.com>
2017-01-23 00:40:39 -0500

    Blockbreaker now has a moving paddle

---
+++ BlockBreaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+	public Paddle paddle;
+	private bool hasStarted = false;
+	private Vector3 paddleToBallVector;
+	// Use this for initialization
+	void Start () {
+		paddleToBallVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!hasStarted){
+			this.transform.position = paddle.transform.position + paddleToBallVector;
+			if(Input.GetMouseButtonDown(0)) {
+				hasStarted = true;				
+				this.rigidbody2D.velocity = new Vector2(2f, 10f);
+			}
+		}		
+	}
+}
---
+++ BlockBreaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Paddle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float mousePosInBlocks = (Input.mousePosition.x / Screen.width * 16);
+		Vector3 paddlePos = new Vector3(8f, this.transform.position.y, 0f);
+		paddlePos.x = Mathf.Clamp(mousePosInBlocks, .5f, 15.5f);
+		this.transform.position = paddlePos;
+	}
+}
